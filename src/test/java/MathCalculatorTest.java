66b08c561896817efb3db4c7e3b52b6440729675
Rahul Baphana <rahul.kbaphana@yahoo.com>
2020-10-12 10:46:56 +0100

    Update test

--- src/test/java/MathCalculatorTest.java
+++ src/test/java/MathCalculatorTest.java
@@ -1,10 +1,15 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static com.prac.calculator.MathCalculator.factorialOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathCalculatorTest {
 
     @Test
-    void foo() {
-        Assertions.assertEquals("foo", "foo");
+    void shouldCalculateFactorialForGivenNumber() {
+        assertEquals(1, factorialOf(0));
+        assertEquals(1, factorialOf(1));
+        assertEquals(6, factorialOf(3));
+        assertEquals(24, factorialOf(4));
     }
 }
